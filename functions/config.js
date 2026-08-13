e74932d412883ac33bb4aae3f30002167c617044
JSKIM <smaz@naver.com>
2026-08-13 22:19:30 +0900

    environment variable

---
+++ functions/config.js
@@ -0,0 +1,17 @@
+// functions/config.js
+export async function onRequest(context) {
+  const SUPABASE_URL = context.env.SUPABASE_URL || "";
+  const SUPABASE_ANON_KEY = context.env.SUPABASE_ANON_KEY || "";
+
+  const body = `
+    export const SUPABASE_URL = "${SUPABASE_URL}";
+    export const SUPABASE_ANON_KEY = "${SUPABASE_ANON_KEY}";
+    `;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/javascript; charset=utf-8",
+      "Cache-Control": "no-store, no-cache, must-revalidate",
+    },
+  });
+}
