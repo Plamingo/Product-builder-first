ab832f4501445ab5297633f7b0d4d5c8a014b72b
JSKIM <smaz@naver.com>
2026-08-13 23:11:25 +0900

    Implement Cloudflare API proxy to secure

--- functions/config.js
+++ functions/config.js
@@ -3,14 +3,16 @@ export async function onRequest(context) {
   const SUPABASE_URL = context.env.SUPABASE_URL || "";
   const SUPABASE_ANON_KEY = context.env.SUPABASE_ANON_KEY || "";
 
-  const body = `export const SUPABASE_URL = ${JSON.stringify(SUPABASE_URL)};
+  console.log(SUPABASE_ANON_KEY)
 
-  export const SUPABASE_ANON_KEY = ${JSON.stringify(SUPABASE_ANON_KEY)};`;
+  // const body = `export const SUPABASE_URL = ${JSON.stringify(SUPABASE_URL)};
 
-  return new Response(body, {
-    headers: {
-      "Content-Type": "application/javascript; charset=utf-8",
-      "Cache-Control": "no-store, no-cache, must-revalidate",
-    },
-  });
+  // export const SUPABASE_ANON_KEY = ${JSON.stringify(SUPABASE_ANON_KEY)};`;
+
+  // return new Response(body, {
+    // headers: {
+      // "Content-Type": "application/javascript; charset=utf-8",
+      // "Cache-Control": "no-store, no-cache, must-revalidate",
+    // },
+  
 }
